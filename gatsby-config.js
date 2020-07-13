be52168dfe7c19c06d9f3c4fb2f18063bf100ef4
Oliver Eisenbarth <oliver.eisenbarth@googlemail.com>
2020-07-13 19:50:18 +0200

    chore: customize theme
    fix: remove potions picture
    
    chore: add .iml to gitignore
    
    refactor: empty static folder
    
    fix: remove picture
    
    refactor: change alt site title, remove instagram link
    
    chore: adapt gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitle: `Foo`,
+    siteTitleAlt: `A foos story`,
+    siteHeadline: `Foo Headline`,
+    siteUrl: ``,
+    siteDescription: `foo foo foo`,
+    siteLanguage: `en`,
+    siteImage: `https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80`,
+    author: ``,
   },
   plugins: [
     {
@@ -26,12 +33,8 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
-          },
+            url: `https://twitter.com/`,
+          }
         ],
       },
     },
@@ -45,25 +48,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `foo-blog - a foos story`,
+        short_name: `foo-blog`,
+        description: ``,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        // icons: [],
       },
     },
     `gatsby-plugin-offline`,
